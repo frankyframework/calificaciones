64b06a309a73f0284dc8bd5a97668727f82d8b86
Ussiel <ussiel@gmail.com>
2021-06-29 17:14:36 -0500

    primer commit

---
+++ loads/init.php
@@ -0,0 +1,17 @@
+<?php
+use Franky\Core\ObserverManager;
+$ObserverManager = new ObserverManager;
+include 'lca.php';
+include 'util.php';
+bindtextdomain("calificaciones", PROJECT_DIR ."/modulos/calificaciones/locale");
+
+
+if (function_exists('bind_textdomain_codeset')) 
+{
+    bind_textdomain_codeset("calificaciones", 'UTF-8');
+}
+
+$ObserverManager->addObserver('login_user','calificaciones_completarTareas');
+
+$MyMetatag->setJs("/modulos/calificaciones/web/js/calificaciones.js");
+?>
